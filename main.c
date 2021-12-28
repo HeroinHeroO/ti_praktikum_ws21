05420b0386229a2d674f51838960937422c4acd7
Aelfric <aelfric.mayer@gmx.at>
2021-12-28 20:08:43 +0100

    add pointers

--- main.c
+++ main.c
@@ -2,20 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
-void fill_array_rnd(short array[], unsigned int arr_size) {
-    srand(time(NULL));
-
+void fill_array_rnd(short *array, unsigned int arr_size) {
     for (int i = 0; i < arr_size; i++) {
-        array[i] = rand() - (RAND_MAX / 2);
+        *(array +i) = rand() - (RAND_MAX / 2);
     }
 }
 
 int main(){
-    short array_eight[8], array_sixteen[16], array_sixtyfour[64];
+    short array_eight[8] = {}, array_sixteen[16] = {}, array_sixtyfour[64] = {};
+    short *array_8p = array_eight;
+    short *array_16p = array_sixteen;
+    short *array_64p = array_sixtyfour;
 
-    fill_array_rnd(array_eight, 8);
-    fill_array_rnd(array_sixteen, 16);
-    fill_array_rnd(array_sixtyfour, 64);
+    srand(time(NULL));
+    fill_array_rnd(array_8p, 8);
+    fill_array_rnd(array_16p, 16);
+    fill_array_rnd(array_64p, 64);
+
+    for (int i = 0; i < 46; i++) {
+        if(i<8)
+        printf("eight: %d: %i\n", i, array_eight[i]);
+        if(i<16)
+        printf("sixteen: %d: %i\n", i, array_sixteen[i]);
+        printf("sixtyfour: %d: %i\n", i, array_sixtyfour[i]);
+    }
 
 
     return 0;
