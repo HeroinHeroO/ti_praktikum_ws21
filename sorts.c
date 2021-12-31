4104c39b9bf9cde6e648c0209bd36cb4c9c236c8
ic21b052 <ic21b052@technikum-wien.at>
2021-12-31 15:58:56 +0100

    mergesort fix

--- sorts.c
+++ sorts.c
@@ -1,57 +1,65 @@
 #include <stdio.h>
 #include "sorts.h"
 
-// verbindet die Subarrays "L" und "M" miteinander.
-void merge(int array[], int p, int q, int r) {
+// sortiert und vereint die Subarrays miteinander.
+void merge(int array[], int first_index, int mid, int last_index) { // die Funktion "merg" benötigt das Array, die erste Indexstelle den Mittelwert und die letzte Indexstelle
 
-    int n1 = q - p + 1;
-    int n2 = r - q;
+    //die zwei Startindexstellen der Arrays
+    int n1 = mid - first_index + 1;
+    int n2 = last_index - mid;
 
-    int L[n1], M[n2];
+    // Deklarierung der Subarrays
+    int sub_array1[n1], sub_array2[n2];
 
+    //Initialisierung der Subarrays
     for (int i = 0; i < n1; i++)
-        L[i] = array[p + i];
+        sub_array1[i] = array[first_index + i];
     for (int j = 0; j < n2; j++)
-        M[j] = array[q + 1 + j];
+        sub_array2[j] = array[mid + 1 + j];
 
     int i, j, k;
     i = 0;
     j = 0;
-    k = p;
+    k = first_index;
 
+    //Sortierung nach aufsteigender Größe
     while (i < n1 && j < n2) {
-        if (L[i] <= M[j]) {
-            array[k] = L[i];
+        if (sub_array1[i] <= sub_array2[j]) {
+            array[k] = sub_array1[i];
             i++;
         } else {
-            array[k] = M[j];
+            array[k] = sub_array2[j];
             j++;
         }
         k++;
     }
 
     while (i < n1) {
-        array[k] = L[i];
+        array[k] = sub_array1[i];
         i++;
         k++;
     }
 
     while (j < n2) {
-        array[k] = M[j];
+        array[k] = sub_array2[j];
         j++;
         k++;
     }
 }
-// das Array in Subarrays geteilt, sortiert und wieder miteinander verbunden.
-void merge_Sort(int array[], int l, int r) {  // die Funktion "merg_Sort" benötigt das Array, eine null und die Array-größe minus 1 (falls kein Element enthalten ist oder das Array nur aus einem Element besteht)
-    if (l < r) { // Überprüfung, ob die größe des Arrays größer als Null ist
+// das Array wird in Subarrays geteilt, sortiert und wieder miteinander verbunden.
+void merge_Sort(int array[], int first_index, int last_index) {  // die Funktion "merg_Sort" benötigt das Array, die erste und letzte Indexstelle
+
+    // Überprüfung, ob nur eine Indexstelle existiert.
+    if (first_index < last_index) {
 
         // die Array-größe wird geteilt
-        int m = l + (r - l) / 2;
+        int mid = first_index +(last_index - first_index) / 2;
 
-        merge_Sort(array, l, m);
-        merge_Sort(array, m + 1, r);
+        // die Funktion "merge_Sort" wird seperat auf beide Teile ausgeführt
+        merge_Sort(array, first_index, mid);
+        merge_Sort(array, mid + 1, last_index);
 
-        merge(array, l, m, r);
+        //führt die Funktion merge aus
+        merge(array, first_index, mid, last_index);
     }
 }
